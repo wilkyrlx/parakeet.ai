6d0550cced6777854d6cab10b9998f27ea366737
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-10-21 10:56:34 -0400

    backend: add express routing

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -7,13 +7,22 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
+import express, { Request, Response } from "express";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express()
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
 });
+
+app.get("/login", (req: Request, res: Response) => {
+    console.log("login")
+    res.send('login')
+});
+
+
+exports.app = functions.https.onRequest(app);
