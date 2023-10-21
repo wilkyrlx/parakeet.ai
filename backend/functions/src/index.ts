7404f0beccc8bc72bac054c7fc58051b45e5b72f
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-10-21 17:20:52 -0400

    feat: add story and get all stories

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -26,64 +26,89 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-const userID = "dummy_user_id"
 
-export const helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send({ message: "Hello from Firebase!" });
-});
+// TODO: change this
+const collectionID = "debug"
+
+function getUserID(req: Request): string {
+    const userID = req.headers.userid;
+    if (userID === undefined) {
+        throw new Error("userID is undefined")
+    } else {
+        return userID as string
+    }
+}
 
 app.get("/", (req: Request, res: Response) => {
-    console.log("base page")
     res.send("main page")
 });
 
-const users: any[] = [];
-
+// Note: v1 complete
 app.post("/register", (req: Request, res: Response) => {
-    const newusername = req.body.user;
-    const newpassword = req.body.pass;
-    const data = { "username": newusername, "password": newpassword }
-    db.collection("test").add(data)
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-        })
+    const userID = getUserID(req)
+    const email = req.body.email;
+    const password = req.body.pass;
+    const data = { "email": email, "password": password }
+
+    db.collection(collectionID).doc(userID).collection('login').add(data)
         .catch((error) => {
-            console.error("Error adding document: ", error);
-        }); 
-    res.send({ message: "Registration Sucessful"} )
+            logger.error("Error adding document: ", error);
+        });
+    res.send({ message: "Registration Successful" })
 });
 
+// TODO
 app.post('/login', (req, res) => {
-    const username = req.body.username;
+    const userID = getUserID(req)
+
+    const email = req.body.email;
     const password = req.body.password;
-    if (users[username] === password) {
-        res.send({ message: 'Login successful' });
-    } else {
-        res.status(401).send({ message: 'Login failed' });
-    }
+
+    const docRef = db.collection(collectionID).doc(userID).collection('login').doc(email);
 });
 
+// TODO
 app.get("/SetPreferences", (req: Request, res: Response) => {
     console.log("editing preferences")
 })
 
+// Note: v1 complete
 app.post("/addStoryToDB", (req: Request, res: Response) => {
-    console.log("adding stories here")
-    res.send({ message: "data" })
-    const textstory = req.body.textstory
-    const title = req.body.title
-    const date = req.body.date
-    const storydata = {
-        textstory,
-        title,
-        date,
+    const userID = getUserID(req)
+
+    const storyData = {
+        "title": req.body.title,
+        "content": req.body.content,
+        "date": req.body.date,
     }
+
+    db.collection(collectionID).doc(userID).collection('stories').add(storyData)
+        .then((docRef) => {
+            logger.log('Story added with ID: ', docRef.id);
+        })
+        .catch((error) => {
+            logger.error('Error adding story: ', error);
+        });
+    res.send({ message: "data added" })
 });
 
+// Note: v1 complete
 app.get("/getAllStories", (req, res) => {
-    console.log("reading the stories")
-    return req.body.stories
+    const userID = getUserID(req)
+
+    logger.log("reading the stories")
+
+    db.collection(collectionID).doc(userID).collection('stories').get()
+        .then((querySnapshot) => {
+            const data = querySnapshot.docs.map((doc) => {
+                return doc.data()
+            })
+            res.send(data)
+        })
+        .catch((error) => {
+            logger.error("Error getting documents: ", error);
+        });
+
 })
 
 exports.app = functions.https.onRequest(app);
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
 import apiService from "../api/ApiService"
+import TextStory from "../types/TextStory"
 
 function HomePage() {
+
+    const currentDate = new Date();
+    const s1 = new TextStory("Title", "Content", currentDate)
+    const s2 = new TextStory("Title2", "Content2", currentDate)
+    const dummyStories: TextStory[] = [s1, s2]
+
     return (
         <div id="home-page">
             Home Page
             <button onClick={() => apiService.register()}>RegisterTest</button>
+            <button onClick={() => apiService.login()}>LoginTest</button>
+            <button onClick={() => apiService.addStoryToDB(s1)}> add Story test</button>
+            <button onClick={() => apiService.getAllStories()}> get Stories test</button>
         </div>
     )
 }
