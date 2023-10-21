e50141cbc1dfefe48f1f7cc297992692cc6251ae
unknown <noahkdesk@gmail.com>
2023-10-21 13:54:01 -0400

    updated backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -19,10 +19,50 @@ export const helloWorld = onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 
-app.get("/login", (req: Request, res: Response) => {
-    console.log("login")
-    res.send('login')
+app.get("/", (req: Request, res: Response) => {
+  console.log("base page")
+  res.send("main page")
 });
 
+const users: any[] = [];
+
+app.post("/register", (req: Request, res: Response) => {
+  const newusername = req.body.newusername;
+  const newpassword = req.body.newpassword;
+  users[newusername] =  newpassword
+  res.send("Registration Sucessful")
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (users[username] === password) {
+    res.send('Login successful');
+  } else {
+    res.status(401).send('Login failed');
+  }
+});
+
+app.get("/SetPreferences", (req: Request, res: Response) => {
+  console.log("editing preferences")
+})
+
+app.post("/addStoryToDB", (req: Request, res: Response) => {
+  console.log("adding stories here")
+  res.send("data")
+  const textstory = req.body.textstory
+  const title = req.body.title
+  const date = req.body.date
+  const storydata = {
+    textstory,
+    title,
+    date,
+  }
+});
+
+app.get("/getAllStories", (req, res) => {
+  console.log("reading the stories")
+  return req.body.stories
+})
 
 exports.app = functions.https.onRequest(app);
