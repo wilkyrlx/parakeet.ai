e02bafc6ae51361ae08bb745d233516d7a9bd520
unknown <noahkdesk@gmail.com>
2023-10-21 17:37:00 -0400

    writing page update

--- frontend/src/pages/WritePage.tsx
+++ frontend/src/pages/WritePage.tsx
@@ -1,11 +1,34 @@
-
+import { SetStateAction, useState } from "react";
+import '../styles/write.css';
 
 function WritePage() {
-    return (
-        <div id="write-page">
-            Write Page
+    const [response, setResponse] = useState('');
+  
+  const handleResponseChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setResponse(e.target.value);
+  }
+  
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Memory of the Day</h1>
+        <div id="prompt">
+          <p>Save something important today!</p>
+          <p>Tell us a story about your favorite food! Why is it so special to you?</p>
         </div>
-    )
+        <textarea
+          id="response"
+          placeholder="Your response..."
+          value={response}
+          onChange={handleResponseChange}
+        />
+        <br /><br />
+        <button id="submit-button" onClick={() => alert(`You responded: ${response}`)}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default WritePage
