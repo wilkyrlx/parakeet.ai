bca0fa30bba331600a26a934377f2149d940c01a
Luna Wang <luna.effervescent85043@gmail.com>
2023-10-21 19:54:44 -0400

    Make textbox on write page disappear after submitting

--- frontend/src/pages/WritePage.tsx
+++ frontend/src/pages/WritePage.tsx
@@ -4,42 +4,58 @@ import apiService from "../api/ApiService";
 import TextStory from "../types/TextStory";
 
 function WritePage() {
-    const [prompt, setPrompt] = useState<string>('Tell us a story about your favorite food! Why is it so special to you?');
-    const [response, setResponse] = useState<string>('');
-      
-    function submitStory(){
-        const currentDate = new Date()
-        const newstory: TextStory = {
-            title: prompt,
-            content: response,
-            date: currentDate,
-        };
-        console.log(newstory);
-        apiService.addStoryToDB(newstory);
-    }; 
+  const [prompt, setPrompt] = useState<string>('Tell us a story about your favorite food! Why is it so special to you?');
+  const [response, setResponse] = useState<string>('');
+  const [submitted, setSubmitted] = useState(false);
 
+  function submitStory() {
+    setSubmitted(true);
+    const currentDate = new Date()
+    const newstory: TextStory = {
+      title: prompt,
+      content: response,
+      date: currentDate,
+    };
+    console.log(newstory);
+    apiService.addStoryToDB(newstory);
+  };
+
+  if (!submitted) {
     return (
-        <div>
-            <div className="container">
-                <h1>Memory of the Day</h1> 
-                <div id="prompt">
-                    <p>Save something important today!</p>
-                    <p>{prompt}</p>
-                </div>
-                
-                <textarea
-                    id="response"
-                    placeholder="Your response..."
-                    value={response}
-                    onChange={(ev) => setResponse(ev.target.value)}
-                />
-                <br /><br />
-                <button id="submit-button" onClick={() => submitStory()}>
-                    Submit
-                </button>
-            </div>
+      <div>
+        <div className="container">
+          <h1>Memory of the Day</h1>
+          <div id="prompt">
+            <p>Save something important today!</p>
+            <p>{prompt}</p>
+          </div>
+
+          <textarea
+            id="response"
+            placeholder="Your response..."
+            value={response}
+            onChange={(ev) => setResponse(ev.target.value)}
+          />
+          <br /><br />
+          <button id="submit-button" onClick={() => submitStory()}>
+            Submit
+          </button>
         </div>
+      </div>
     );
+  }
+  else {
+    return (
+      <div>
+        <div className="container">
+          <h1>Memory of the Day</h1>
+          <div id="prompt">
+            <p>Thank you for submitting!</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WritePage
