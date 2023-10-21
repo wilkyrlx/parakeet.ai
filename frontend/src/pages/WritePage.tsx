295511948a304f6cfe9c8a163fa0c4c2ef8938a8
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-10-21 17:54:24 -0400

    refactor: writePage

--- frontend/src/pages/WritePage.tsx
+++ frontend/src/pages/WritePage.tsx
@@ -3,32 +3,29 @@ import '../styles/write.css';
 
 function WritePage() {
     const [response, setResponse] = useState('');
-  
-  const handleResponseChange = (e: { target: { value: SetStateAction<string>; }; }) => {
-    setResponse(e.target.value);
-  }
-  
-  return (
-    <div className="App">
-      <div className="container">
-        <h1>Memory of the Day</h1>
-        <div id="prompt">
-          <p>Save something important today!</p>
-          <p>Tell us a story about your favorite food! Why is it so special to you?</p>
+
+
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>Memory of the Day</h1>
+                <div id="prompt">
+                    <p>Save something important today!</p>
+                    <p>Tell us a story about your favorite food! Why is it so special to you?</p>
+                </div>
+                <textarea
+                    id="response"
+                    placeholder="Your response..."
+                    value={response}
+                    onChange={(ev) => setResponse(ev.target.value)}
+                />
+                <br /><br />
+                <button id="submit-button" onClick={() => alert(`You responded: ${response}`)}>
+                    Submit
+                </button>
+            </div>
         </div>
-        <textarea
-          id="response"
-          placeholder="Your response..."
-          value={response}
-          onChange={handleResponseChange}
-        />
-        <br /><br />
-        <button id="submit-button" onClick={() => alert(`You responded: ${response}`)}>
-          Submit
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default WritePage
